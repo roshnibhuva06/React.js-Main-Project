9dd781f4e8248c33af8e04331d63756af8441848
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-08-11 11:34:06 +0530

    Update todoSlice.js

--- redux-todo/src/Features/todoSlice.js
+++ redux-todo/src/Features/todoSlice.js
@@ -1,13 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState = []
 const todoSlice = createSlice({
     name: "Todos",
     initialState : [],
     reducers: {
         Addtodo: (state, action) => {
-            state.push({ text: action.payload, completed: false })
+            state.push({ id: Date.now(), text: action.payload })
         },
         Remove: (state, action) => {
             const index = state.findIndex((todo) => todo.id === action.payload);
@@ -15,15 +13,16 @@ const todoSlice = createSlice({
                 state.splice(index, 1);
             }
         },
-        Updatetodo: (state, action) =>{
-            const { id ,text} = action.payload;
-            const index = state.findIndex((todo) => todo.id === id);
-            if (index !== -1) {
-              state[index].text = Text;
-            }
-        }
+       Updatetodo: (state, action) =>{
+    const { id, text } = action.payload;
+    const index = state.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      state[index].text = text;
+    }
+}
+
     },
 });
 
-export default todoSlice.reducer
 export const { Addtodo , Remove ,Updatetodo} = todoSlice.actions;
+export default todoSlice.reducer;
